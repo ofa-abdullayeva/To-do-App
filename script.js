b3beef4d5aacc519827680463c81bb7a12419a60
ofa-abdullayeva <abdullayevaofa25@gmail.com>
2021-08-08 21:31:39 +0400

    To do App

---
+++ script.js
@@ -0,0 +1,4 @@
+const form = document.querySelector('form') ;
+const input = document.querySelector('#txtTaskName');
+const btnDeleteAll = document.querySelector('btnDeleteAll');
+const tasklist = document.querySelector('#task-list');
